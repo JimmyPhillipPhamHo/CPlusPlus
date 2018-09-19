6580367ee804b97e1e643504ac30d229776bdd66
Noble115 <jimmyphamho@yahoo.com>
2018-09-19 10:07:46 -0500

    Triangle Cal Update 3
    Add Isosceles Trinagle
    Fix bug in main function
    Add more code

--- Chapter 4-5/TriangleCalculator/TriangleCalculator/IsoscelesTrianlge.cpp
+++ Chapter 4-5/TriangleCalculator/TriangleCalculator/IsoscelesTrianlge.cpp
@@ -6,22 +6,30 @@
 
 using namespace std;
 
-double areaCalculate(double aSide, double bSide)
+double areaIsoscelesCal(double aSide, double baseSide)
 {
 	double areaResult;
-	areaResult = (aSide * bSide) / 2;
+	double height;
+	double basehalf;
+	basehalf = baseSide /2;
+
+	height = (aSide * aSide) - (basehalf * basehalf) ;
+	height = sqrt(height);
+	
+	areaResult = 0.5 * baseSide * height;
 	return areaResult;
 
 }
 
-double perimeterCalculate(double aSide, double bSide, double cSide)
+
+double perimeterIsoscelesCal(double aSide, double bSide)
 {
 	double perResult;
-	perResult = aSide + bSide + cSide;
+	perResult = (2 * aSide) + bSide;
 	return perResult;
 }
 
-double missingSideA(double bSide, double cSide)
+double MissingSideA(double bSide, double cSide)
 {
 	double side_A_Result;
 	side_A_Result = (bSide * bSide) + (cSide * cSide);
@@ -29,7 +37,7 @@ double missingSideA(double bSide, double cSide)
 	return side_A_Result;
 }
 
-double missingSideB(double aSide, double cSide)
+double MissingSideB(double aSide, double cSide)
 {
 	double side_B_Result;
 	side_B_Result = (aSide * aSide) + (cSide * cSide);
@@ -37,7 +45,7 @@ double missingSideB(double aSide, double cSide)
 	return side_B_Result;
 }
 
-double missingSideC(double aSide, double bSide)
+double MissingSideC(double aSide, double bSide)
 {
 	double side_C_Result;
 	side_C_Result = (aSide * aSide) + (bSide * bSide);
@@ -45,16 +53,18 @@ double missingSideC(double aSide, double bSide)
 	return side_C_Result;
 }
 
-void rightTriangle()
+void isoscelesTriangle()
 {
+	double test;
 	//declare variables
 	int continueSolve;
 	int chooseSolve;
-	double solution;
+	double Solution;
+	double baseSide;
 	double aSide;
 	double bSide;
 	double cSide;
-	cout << "You have choice Right Trinagle" << endl;
+	cout << "You have choice Isosceles Trinagle" << endl;
 	cout << "What do you what to solve for" << endl;
 	cout << "Solve for Area = 1, Solve for Preimeter = 2, Solve for Missing Side = 3" << endl;
 	cout << "Enter your choose here: ";
@@ -69,13 +79,15 @@ void rightTriangle()
 			cout << "Type the number for side A and side B of the triangle to get the area." << endl;
 			cout << "Please enter side A here: ";
 			cin >> aSide;
-			cout << "Please enter side B here: ";
-			cin >> bSide;
+			cout << "Please enter side baseSide here: ";
+			cin >> baseSide;
 
-			solution = areaCalculate(aSide, bSide);
-			cout << "The Area of Triangle is: " << solution << endl;
+			Solution = areaIsoscelesCal(aSide, baseSide);
+			cout << "The Area of Isosceles Triangle is: " << Solution << endl;
 
-			cout << "Would you like to continue solving area for right Triangle?" << endl;
+			
+
+			cout << "Would you like to continue solving area for isosceles Triangle?" << endl;
 			cout << "Type 1 for yes or 2 for no" << endl;
 			cout << "Please enter your choice here: ";
 			cin >> continueSolve;
@@ -95,13 +107,11 @@ void rightTriangle()
 			cin >> aSide;
 			cout << "Please enter side B here: ";
 			cin >> bSide;
-			cout << "Please enter side C here: ";
-			cin >> cSide;
 
-			solution = perimeterCalculate(aSide, bSide, cSide);
-			cout << "The Perimeter of Triangle is: " << solution << endl;
+			Solution = perimeterIsoscelesCal(aSide, bSide);
+			cout << "The Perimeter of Triangle is: " << Solution << endl;
 
-			cout << "Would you like to continue solving perimeter for right Triangle?" << endl;
+			cout << "Would you like to continue solving perimeter for isosceles Triangle?" << endl;
 			cout << "Type 1 for yes or 2 for no" << endl;
 			cout << "Please enter your choice here: ";
 			cin >> continueSolve;
@@ -125,8 +135,8 @@ void rightTriangle()
 			cin >> bSide;
 			cout << "Please enter side C here: ";
 			cin >> cSide;
-			solution = missingSideA(bSide, cSide);
-			cout << "Missing Side A for the right Triangle is: " << solution << endl;
+			Solution = MissingSideA(bSide, cSide);
+			cout << "Missing Side A for the right Triangle is: " << Solution << endl;
 		}
 		else if (chooseSolve == 2)
 		{
@@ -135,8 +145,8 @@ void rightTriangle()
 			cin >> aSide;
 			cout << "Please enter side C here: ";
 			cin >> cSide;
-			solution = missingSideA(aSide, cSide);
-			cout << "Missing Side B for the right Triangle is: " << solution << endl;
+			Solution = MissingSideA(aSide, cSide);
+			cout << "Missing Side B for the right Triangle is: " << Solution << endl;
 		}
 		else if (chooseSolve == 3)
 		{
@@ -145,10 +155,10 @@ void rightTriangle()
 			cin >> aSide;
 			cout << "Please enter side B here: ";
 			cin >> bSide;
-			solution = missingSideA(aSide, bSide);
-			cout << "Missing Side C for the right Triangle is: " << solution << endl;
+			Solution = MissingSideA(aSide, bSide);
+			cout << "Missing Side C for the right Triangle is: " << Solution << endl;
 		}
-			
+
 
 	}
 
